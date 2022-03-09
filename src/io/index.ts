17030f51ad7adffe2f244561a5a225f37c5d94e3
Rob <rob@wilkie.io>
2022-03-09 11:50:47 +0000

    0.1.1: line-endings

--- src/io/index.ts
+++ src/io/index.ts
@@ -1,5 +1,7 @@
-import {Dirent, readdirSync, readFileSync} from "fs";
+import {Dirent, readdirSync, readFileSync, mkdir, writeFile} from "fs";
 import {extname, join} from "path";
+
+const getDirName = require("path").dirname
 
 import {getNameObj} from "../strings";
 import {logger} from "../logger";
@@ -18,4 +20,21 @@ export const listAllSVG = (path: string): SVGFile[] => {
 
     logger.info(`Found ${files.length} SVGs in ${path}`);
     return files;
+}
+
+export const saveFile = (path: string, contents: string) => {
+    mkdir(getDirName(path), {recursive: true}, (err) => {
+        if (err) {
+            logger.error(err.message);
+            return;
+        }
+
+        writeFile(path, contents, err => {
+            if (err) {
+                logger.error(err.message);
+                return;
+            }
+            logger.info(`Saved file to ${path}`);
+        })
+    })
 }
