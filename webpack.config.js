509edddf1062bafbb6bbc75cb5510301e8200c5f
Rob <rob@wilkie.io>
2022-03-16 02:29:57 +0000

    0.2.17
    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path")
 const nodeExternals = require("webpack-node-externals")
+const Shebang = require("webpack-shebang-plugin")
 
 module.exports = {
     mode: "production",
     target: "node",
     externals: [nodeExternals()],
+    plugins: [new Shebang({banner: "#!/usr/bin/env node", raw: true})],
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "dist"),
@@ -13,6 +15,15 @@ module.exports = {
     optimization: {
         minimize: true
     },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    }
 }
 
 // (28,672 bytes)
